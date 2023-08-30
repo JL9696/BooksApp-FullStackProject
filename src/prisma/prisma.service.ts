eba482632a766fa6fa1c7f63861148ede2e197b2
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-08-30 18:49:38 +0200

    add schema prisma, add prisma models, connect to mysql database

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  private _user: any;
+  public get user(): any {
+    return this._user;
+  }
+  public set user(value: any) {
+    this._user = value;
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    process.on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
